8b881a0118f53538bd132c561027889a70c2772a
william_doyle <mcbidebilly@gmail.com>
2023-10-04 19:56:44 -0400

    WrappedContractsDemo begin

---
+++ contracts/WrappedContractDemo.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// there are some questions about StakedModeration.sol which can be answered by 
+// creating and deploying a simple "proof of concept" contract. This contract
+// will be deployed on Milkomeda C1 and will be used to test the User Experience
+// and Developer Experience of wrapped smart contracts. I am also currious about
+// what Mainnet specific data can be made available to the contract. For example,
+// can the contract on Milkomeda access the stake balance of the account that 
+// called it? Can it query the stake balance of any L1 account? 
+
+contract WrappedContractDemo {
+
+}
